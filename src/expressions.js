f96ea1b653d7a57a0cb7c4465cce962b5a6fff13
lohannlehenaff <lohannlehenaff@gmail.com>
2025-09-15 23:40:08 +0200

    Mise à jour App.jsx et expressions.js

---
+++ src/expressions.js
@@ -0,0 +1,26 @@
+const EXPRESSIONS = [
+  {
+    phrase: "Tirer les ficelles",
+    origin: "Du monde du théâtre de marionnettes",
+    options: [
+      "Du monde du théâtre de marionnettes",
+      "De la pêche",
+      "De la couture",
+      "Du tir à l’arc"
+    ],
+    explanation: "L’expression vient du théâtre de marionnettes, où le marionnettiste contrôle les personnages en tirant les ficelles. Par extension, cela désigne une personne qui dirige discrètement les événements."
+  },
+  {
+    phrase: "Se mettre sur son trente-et-un",
+    origin: "D’un tissu luxueux appelé 'trentain' au XIXe siècle",
+    options: [
+      "D’un tissu luxueux appelé 'trentain' au XIXe siècle",
+      "Parce que les rois changeaient 31 fois de costume",
+      "En référence au calendrier républicain",
+      "À cause d’un jeu de cartes"
+    ],
+    explanation: "Au XIXe siècle, le 'trentain' était un tissu très chic. Porter ce tissu signifiait être bien habillé, d’où l’expression actuelle pour désigner une tenue élégante."
+  }
+];
+
+export default EXPRESSIONS;
